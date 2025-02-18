d90860714de2eac87fc3697289da91333759bac1
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2025-02-18 14:49:35 +0200

    arrays

--- oop/syntax-and-basics/src/main.ts
+++ oop/syntax-and-basics/src/main.ts
@@ -72,7 +72,7 @@ numbersArray[0].toFixed();
 const person = {
   name: "omer",
   age: 14,
-  hobbies: ["swim"],
+  isActive: true,
 };
 
 const value = getValueByKey(person, "age");
@@ -81,3 +81,80 @@ console.log(value);
 function getValueByKey<TObj, TKey extends keyof TObj>(obj: TObj, key: TKey) {
   return obj[key];
 }
+
+function createArray<T>(): T[] {
+  return [];
+}
+
+const stringArr = createArray<string>();
+
+// any vs unkown vs never
+let myAny: any = 1;
+
+myAny.forEach();
+myAny.toFixed();
+myAny.omer = "mama";
+
+let myUnknown: unknown = 1;
+
+if (typeof myUnknown === "number") {
+  myUnknown.toFixed();
+}
+
+// if (myUnknown && typeof myUnknown === "object") {
+//   myUnknown.forEach(() => {});
+// }
+
+if (Array.isArray(myUnknown)) {
+  myUnknown.forEach(() => {});
+}
+
+if (myUnknown && typeof myUnknown === "object" && "omer" in myUnknown) {
+  myUnknown.omer = "mama";
+}
+
+myUnknown.omer = "mama";
+
+let myNever: never = undefined;
+
+if (typeof myNever === "string") {
+  myNever = "ssss";
+}
+
+myNever = undefined;
+myNever = null;
+
+type Plan = "standard" | "gold" | "platinum" | "baba";
+
+function checkUserPlan(plan: Plan, blogAmount: number) {
+  switch (plan) {
+    case "standard":
+      plan;
+      return blogAmount < 1;
+    case "gold":
+      plan;
+      return blogAmount < 3;
+    case "platinum":
+      plan;
+      return true;
+    default:
+      const _unreachable: never = plan;
+      throw new Error("unreachable!");
+  }
+}
+
+const res = checkUserPlan("standard", 1);
+
+/**
+ * implement custom array methods (see myMap inclass example, ignore TS):
+ *
+ * 1. forEach - retrun nothing
+ * 2. filter - array
+ * 3. find - item | null
+ * 4. findIndex - number | -1 (in case we dont find any)
+ * 5. every - boolean
+ * 6. some - boolean
+ * 7. includes - boolean
+ * 8. Bonus: Reduce - see docs.
+ * 9. whetver you like...
+ */
